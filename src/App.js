d7959670f9231c24a8ae8ae5f3ffde553ff718dc
Khach145 <khach.galstyan@gmail.com>
2022-02-25 22:30:09 +0400

    initial

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import { Web3ReactProvider } from '@web3-react/core';
+import { Web3Provider } from '@ethersproject/providers';
+import Web3ReactConnectionComponent from './Web3ReactConnectionComponent';
+window.onload = function() {
+	localStorage.clear();
+};
+
+function App() {
+	const getLibrary = (provider) => {
+		const library = new Web3Provider(provider, 'any');
+		library.pollingInterval = 15000;
+		return library;
+	};
+
+	return (
+		<Web3ReactProvider getLibrary={getLibrary}>
+			<div className="flex space-x-3">
+				<Web3ReactConnectionComponent />
+			</div>
+		</Web3ReactProvider>
+	);
+}
+
+export default App;
