4023e2996a67ff946cf7a55e6548cebfdbfd0e33
Ievgen Cherkaskyi <ievgen.cherkaskyi@gmail.com>
2022-04-20 16:42:27 +0300

    Remove map

--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -1,39 +1,21 @@
-import React, {useState} from 'react';
-import HeaderLogo from '../headerLogo/HeaderLogo';
-import Navbar from '../navbar/Navbar';
-import Map from '../map/Map';
-import InnerNav from '../InnerNav/InnerNav';
-import MinimapMenu from '../minimapMenu/MinimapMenu';
-import { useTranslation } from 'react-i18next';
-import i18n from 'i18next'
-import Hexagon from '../hex/Hexagon';
-import Braces from '../../braces/Braces';
-import HexGlobe from '../hex/HexGlobe'
-import './main.css'
-import { renderGlobe } from '../hex/renderGlobe';
+import React from "react";
+import HeaderLogo from "../headerLogo/HeaderLogo";
+import Navbar from "../navbar/Navbar";
+import InnerNav from "../InnerNav/InnerNav";
+import MinimapMenu from "../minimapMenu/MinimapMenu";
+import Braces from "../../braces/Braces";
+import "./main.css";
 
 const Main = () => {
-    const { t } = useTranslation();
-    
-    const onChange = (event) => {
-        i18n.changeLanguage(event.target.value);
-      };
+  return (
+    <div className="main">
+      <HeaderLogo />
+      <Navbar />
+      <Braces />
+      <InnerNav />
+      <MinimapMenu />
+    </div>
+  );
+};
 
-    if(!window.rendered) {
-        // renderGlobe()
-        window.rendered = true
-    }
-
-    return (
-        <div className="main">
-        <HeaderLogo />
-        <Navbar />
-        {/* <HexGlobe /> */}
-        <Braces />
-        <InnerNav />
-        <MinimapMenu />
-        </div>
-    )
-}
-
-export default Main
+export default Main;
--- src/index.js
+++ src/index.js
@@ -1,24 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import {BrowserRouter as Router,  Routes, Route, useNavigate} from 'react-router-dom'
-import './i18n'
-import 'semantic-ui-css/semantic.min.css'
-import Preloader from './utils/Preloader'
-import {moralisProvider} from 'react-moralis'
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter as Router } from "react-router-dom";
+import "./i18n";
+import "semantic-ui-css/semantic.min.css";
+import Preloader from "./utils/Preloader";
 
 ReactDOM.render(
   // <React.StrictMode>
   <React.Suspense fallback="">
     <Preloader />
     <Router>
-    <App />
+      <App />
     </Router>
   </React.Suspense>,
   // </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
